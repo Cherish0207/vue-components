a73f2b3563436dfb093f1cededfc29793f911140
Cherish0207 <cheshire_1207@foxmail.com>
2021-01-23 09:32:25 +0800

    .

---
+++ components/Grandson1.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    孙子:{{ value }}
+    <!-- 调用父组件的input事件 -->
+    <button @click="$parent.$emit('input', 200)">更改</button>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+    },
+  },
+};
+</script>
--- components/son1.vue
+++ components/son1.vue
@@ -3,10 +3,15 @@
     子组件1: {{ value }}
     <!-- 触发的事件只能是input -->
     <button @click="$emit('input', 20)">更改</button>
+    <Grandson1 :value="value"></Grandson1>
   </div>
 </template>
 <script>
+import Grandson1 from "./Grandson1";
 export default {
+  components: {
+    Grandson1,
+  },
   props: {
     value: {
       // 接收到的属性名只能叫value
